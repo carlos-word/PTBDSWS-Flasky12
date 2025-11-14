fb62643f5635f61002f004262965da0df91805eb
Carlos Martins <carlosouza.ces@gmail.com>
2025-11-13 22:00:39 -0300

    Update errors.py

--- app/main/errors.py
+++ app/main/errors.py
@@ -2,9 +2,5 @@ from flask import render_template
 from . import main
 
 @main.app_errorhandler(404)
-def page_not_found(e):
+def page_not_founed(e):
     return render_template('404.html'), 404
-
-@main.app_errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
