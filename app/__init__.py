f1474dfe2aa5eaa326fad13cb884e31c6e2d09ea
Carlos Martins <carlosouza.ces@gmail.com>
2025-11-20 15:04:53 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,23 @@
+import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import config
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
-
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -20,8 +28,12 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
