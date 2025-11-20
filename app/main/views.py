ec3b872589e714777b728281335d92d5fd34c938
Carlos Martins <carlosouza.ces@gmail.com>
2025-11-20 14:59:12 -0300

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,13 @@
 from flask import render_template
+from flask_login import current_user
 from . import main
-
+from app.models import User
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    users = None
+    if current_user.is_authenticated:
+        users = User.query.all()
+
+    return render_template('index.html', users=users)
+
